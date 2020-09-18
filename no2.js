0aee2bd2a1c63de62a16cf2b96d3bb5c6c138655
Dzulfanni <belajarayok866@gmail.com>
2020-09-18 22:08:27 +0700

    first commit

---
+++ no2.js
@@ -0,0 +1,35 @@
+const asalSantri = {
+    Sidik: "Lampung",
+    Wandi: "Prabumulih",
+    Rahmad: "Medan",
+    Usamah: "Ceribon",
+    Syofyan: "Purwokerto",
+    Aldi: "Kendari",
+    Kardi: "Lombok"
+}
+var
+ (
+    a,
+    b,
+    c,
+    d,
+    e,
+    f,
+    g,
+    h,
+    i,
+    j,
+    k,
+    l,
+    m,
+    n
+ ) = asalSantri 
+{
+
+    document.write(b);
+    document.write(d);
+    document.write(f);
+}
+console.log(Syofyan);
+console.log(Aldi);
+console.log(Kardi);
